997166b7c5d1c1e3b2681d7ef36299a99f12d6c1
catalin.voineag <catalin.voineag@onthebeach.co.uk>
2021-04-30 16:29:34 +0100

    Implement Background and ship
    This MR implements a simple black background when the window opens and
    draws a ship in the middle of the screen
    
    The window has will have the same resolution as the user's monitor and
    can be closed by pressing the Escape key.
    
    The ship is drawn in the middle of the screen and is a sprite of 40 by
    40 px.

--- main.cpp
+++ main.cpp
@@ -7,7 +7,55 @@ using namespace sf;
 
 int main()
 {
-	std::cout << "Asteroids Game" << std::endl;
+
+	Vector2f resolution;
+	resolution.x = VideoMode::getDesktopMode().width;
+	resolution.y = VideoMode::getDesktopMode().height;
+
+	RenderWindow window(VideoMode(resolution.x, resolution.y), "Asteroids", Style::Fullscreen);
+
+	Texture backgroundTexture;
+	backgroundTexture.loadFromFile("graphics/background.png");
+
+	Texture shipTexture;
+	shipTexture.loadFromFile("graphics/ship.png");
+
+	Sprite shipSprite;
+	shipSprite.setTexture(shipTexture);
+
+	int windowHalfWidht = resolution.x / 2;
+	int windowHalfHeight = resolution.y / 2;
+	int shipHalfWidht = shipSprite.getGlobalBounds().width / 2;
+	int shipHalfHeight = shipSprite.getGlobalBounds().height / 2;
+
+	shipSprite.setPosition(
+			windowHalfWidht - shipHalfWidht, windowHalfHeight - shipHalfHeight);
+	
+	Sprite backgroundSprite;
+	backgroundSprite.setTexture(backgroundTexture);
+	backgroundSprite.setPosition(0, 0);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+		if (Keyboard::isKeyPressed(Keyboard::Escape))
+		{
+			window.close();
+		}
+
+		window.clear();
+
+		window.draw(backgroundSprite);
+		window.draw(shipSprite);
+
+		window.display();
+	}
 	return 0;
 }
 
