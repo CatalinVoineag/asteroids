f25bdfcbd855b10a3e7df5bc5a5720d9bbc874b8
catalin.voineag <catalin.voineag@onthebeach.co.uk>
2021-04-30 12:51:07 +0100

    Asteroids hello world

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <sstream>
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+
+using namespace sf;
+
+int main()
+{
+	std::cout << "Asteroids Game" << std::endl;
+	return 0;
+}
+
